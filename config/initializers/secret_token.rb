7d7a12ae0588501552b9b3380f3156d6896a0431
vagrant <vagrant@precise32.(none)>
2014-03-18 09:49:30 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FitnessDiary::Application.config.secret_key_base = '77a8c953e40e541dd6437fcff0a7daf1d23a109e6b8f094e699de719e3688e3fa4580d9662ce65a771b56c9a31c7e0a38647ab741c550a4f6e1afdc29f34e897'
